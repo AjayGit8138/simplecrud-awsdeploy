950598fdd907ac3f19b437f806f5c4f3b92527c6
Ajay8138 <prasathmajay@gmail.com>
2026-03-10 20:46:08 +0530

    env file for mongo added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://prasathmajay_db_user:k27xYAOy8wW773nS@cluster0.djesdhl.mongodb.net/crudapp')
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.djesdhl.mongodb.net/crudapp`)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
